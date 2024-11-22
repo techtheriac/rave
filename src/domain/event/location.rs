01e01727d5024adf0c2d6bf3d37fd80cc9e5bc46
Jezreel Franklin <mavyjayann@gmail.com>
2024-11-22 11:34:35 +0100

    Add coord

--- src/domain/event/location.rs
+++ src/domain/event/location.rs
@@ -1,6 +1,7 @@
 pub struct Location {
   pub location_address: String,
-  pub location_status: LocationStatus
+  pub location_status: LocationStatus,
+  pub coord: String
 }
 
 pub enum LocationStatus {
